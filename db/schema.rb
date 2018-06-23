c0d99f4700f6ef650588c01fe479dfe6022abba9
Srinidhi G S <srini.satti+lwt@gmail.com>
2018-06-23 22:01:03 +0530

    add btn for questions

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 ActiveRecord::Schema.define(version: 20180623151416) do
 
   create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "first_name"
+    t.string "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
